9e3f7ab6202ed941a12f81b42616f0d7c53e8d33
tomzumbi <114890330+tomzumbi@users.noreply.github.com>
2022-11-01 20:53:17 +0700

    Add files via upload

---
+++ hienthiloichao/src/HienThiLoiChao.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class HienThiLoiChao {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("nhap ten cua ban");
+        String name = scanner.nextLine();
+        System.out.println("xin chao " +name);
+    }
+}
